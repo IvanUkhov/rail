8173d7a8ea97e70a38c1729b14c65869c10b2abb
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-07-15 13:00:59 +0200

    Refactored the template generator

--- lib/rail/generator.rb
+++ lib/rail/generator.rb
@@ -1,76 +1,47 @@
-require 'erb'
-require 'ostruct'
-require 'fileutils'
+require 'generator'
 
 module Rail
-  class Generator
-    attr_reader :root_dir, :template_dir
+  class Generator < ::Generator
+    Error = Class.new(StandardError)
+
+    FILES = [
+      'app/assets/javascripts/application.js.coffee',
+      'app/assets/stylesheets/application.css.scss',
+      'app/helpers/application_helper.rb',
+      'app/views/layouts/application.html.haml.erb',
+      'config/application.rb.erb',
+      'config.ru.erb',
+      'Gemfile',
+      'public/favicon.png',
+      'Rakefile.erb'
+    ]
 
     def initialize(options)
-      @root_dir = options.fetch(:root_dir)
-      @template_dir = options.fetch(:template_dir)
+      root_dir = options.fetch(:root_dir)
+      template_dir = File.expand_path('../templates', __FILE__)
+
+      project_name = root_dir.split('/').last
+        .gsub(/^[^a-zA-Z]*/, '')
+        .gsub(/[^\w]/, '')
+        .gsub(/^\w|_\w/, &:upcase)
+        .gsub(/_+/, ' ')
+
+      class_name = project_name.gsub(' ', '')
+
+      @locals = { class_name: class_name, project_name: project_name }
+
+      raise Error('The project name is invalid.') if class_name.empty?
+
+      super(root_dir: root_dir, template_dir: template_dir)
     end
 
-    def process(files, locals = {})
-      context = OpenStruct.new(locals)
-      files.each { |file| process_one(file, context) }
-    end
-
-    private
-
-    def process_one(file, context)
-      source = find(file)
-      destination = route(file)
-
-      directory = File.dirname(destination)
-      unless File.directory?(directory)
-        report("Creating a directory: #{ directory }")
-        make(directory)
-      end
-
-      report("Creating a file: #{ destination }")
-      if template?(file)
-        dump(transform(load(source), context), destination)
-      else
-        copy(source, destination)
-      end
-    end
-
-    def template?(file)
-      File.extname(file) == '.erb'
-    end
-
-    def find(file)
-      File.join(template_dir, file)
-    end
-
-    def route(file)
-      File.join(root_dir, file).gsub(/\.erb$/, '')
+    def run
+      raise Error('The directory already exists.') if File.directory?(root_dir)
+      super(FILES, @locals)
     end
 
     def report(message)
-      puts message
-    end
-
-    def make(destination)
-      FileUtils.mkdir_p(destination)
-    end
-
-    def copy(source, destination)
-      FileUtils.cp(source, destination)
-    end
-
-    def load(source)
-      File.read(source)
-    end
-
-    def dump(content, destination)
-      File.open(destination, 'w') { |file| file.write(content) }
-    end
-
-    def transform(content, context)
-      context.singleton_class.class_eval('def get_binding; binding; end')
-      ERB.new(content).result(context.get_binding)
+      puts "Creating '#{ message }'..."
     end
   end
 end
